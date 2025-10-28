ce925a67a59847128a44e54a2a28045200db7422
ionutbalanel <ionelbalanel@gmail.com>
2025-10-28 19:30:45 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -82,15 +82,16 @@ function showVerdict(angle) {
     let verdict = "";
 
     if (angle <= 5.7) {
-        verdict = `Unghi: ${angle.toFixed(2)}° → ✅ Accesibil`;
+        verdict = `Unghi: ${angle.toFixed(2)}° → ✅ Accesibil / Accessible`;
         output.className = "result green";
     } else if (angle <= 15) {
-        verdict = `Unghi: ${angle.toFixed(2)}° → ⚠️ Parțial accesibil`;
+        verdict = `Unghi: ${angle.toFixed(2)}° → ⚠️ Parțial accesibil / Partially accessible`;
         output.className = "result yellow";
     } else {
-        verdict = `Unghi: ${angle.toFixed(2)}° → ❌ Inaccesibil`;
+        verdict = `Unghi: ${angle.toFixed(2)}° → ❌ Inaccesibil / Inaccessible`;
         output.className = "result red";
     }
 
     output.textContent = verdict;
+
 }
